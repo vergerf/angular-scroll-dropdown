67784895214b8536c9896de7c60fa2a747a9cb42
vergerf <florian.verger.epitech@gmail.com>
2014-11-20 04:24:42 -0800

    [REF] refactoring (no need status.isopen anymore) + precision increase.

--- angular-scroll-dropdown.js
+++ angular-scroll-dropdown.js
@@ -9,20 +9,13 @@
 
   'use strict';
 
-// Directive which will be associate to the dropdown element
-angular.module('angular-scroll-dropdown', ['ui.bootstrap'])
-.directive('dropdownscroll', [function() {
-  return {
-    restrict: 'C',
-    controller: function($scope, $element) {
-      $scope.status = {
-        isopen : false
-      };
-    },
-    link: function (scope, elm, attrs) {
-      var button = elm.find('.dropdown-toggle');
-      var dropdown = elm.find('.dropdown-menu-scoll');
-      var content = elm.find('.btn-group');
+  angular.module('angular-scroll-dropdown', ['ui.bootstrap'])
+  .directive('dropdownscroll', [function() {
+    return {
+      restrict: 'C',
+      link: function (scope, elm) {
+        var button = elm.find('.dropdown-toggle');
+        var dropdown = elm.find('.dropdown-menu-scoll');
 
         // change dropdown position if click on button
         button.bind('click', function() {
@@ -41,28 +34,36 @@ angular.module('angular-scroll-dropdown', ['ui.bootstrap'])
         scope.$on('contentScroll:scrolling', function (event, scroll) {
           var dropdown = elm.find('.dropdown-menu-scoll:visible');
           if (dropdown.length !== 0) {
-            dropdown.css('top', (dropdown.offset().top - scroll.scrollValue) + "px");
-            if (dropdown.offset().top < scroll.top || dropdown.offset().top > scroll.bottom) {
-              scope.status.isopen = false;
-              scope.$digest();
+
+            var dropDownTopInBottom = button.offset().top + button.outerHeight();
+            var dropDownTopInTop = button.offset().top;
+
+            if ($(window).height() < (dropDownTopInBottom + dropdown.height())) {
+              dropdown.css('top', (dropDownTopInTop - dropdown.outerHeight()) + "px");
+            } else {
+              dropdown.css('top', dropDownTopInBottom + "px");
+            }
+            dropdown.css('left', button.offset().left + "px");
+
+            if (dropDownTopInTop < scroll.top || dropDownTopInBottom > scroll.bottom) {
+              button.click();
             }
           }
         });
       },
     };
   }])
-// Directive which will be associate to the overflow div element
 .directive('contentscroll', function() {
   return {
     restrict: 'C',
-    link: function(scope, elm, attr) {
-      var raw = elm[0];
-      var oldScrollTop = raw.scrollTop;
-
+    link: function(scope, elm) {
           // send message to children if scrolling
           elm.bind('scroll', function() {
-            scope.$broadcast('contentScroll:scrolling', {top: elm.offset().top, bottom: (elm.offset().top + elm.height()), scrollValue : (raw.scrollTop - oldScrollTop)});
-            oldScrollTop = raw.scrollTop;
+            scope.$broadcast('contentScroll:scrolling',
+            {
+              top: elm.offset().top,
+              bottom: (elm.offset().top + elm.height()),
+            });
           });
         },
       };
