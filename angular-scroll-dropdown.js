8f0d3bb0ede1424bd2670f4f16ac9678a059cfa1
Stav Milstein <stavmil@gmail.com>
2015-08-10 10:21:57 +0300

    change scroll broadcast parameter

--- angular-scroll-dropdown.js
+++ angular-scroll-dropdown.js
@@ -5,12 +5,12 @@
  *
  */
 
-(function () {
+(function() {
 
     'use strict';
 
     angular.module('angular-scroll-dropdown', ['ui.bootstrap'])
-        .directive('dropdownscroll', ['$window', function ($window) {
+        .directive('dropdownscroll', ['$window', function($window) {
             return {
                 restrict: 'C',
                 link: function (scope, elm) {
@@ -18,14 +18,14 @@
                     var dropdown = elm.find('.dropdown-menu-scoll');
 
                     // change dropdown position if click on button
-                    button.bind('click', function () {
-                        var dropDownTopInBottom = button.offset().top + button.outerHeight();
-                        var dropDownTopInTop = button.offset().top;
+                    button.bind('click', function() {
+                        var dropDownTopInBottom = button.offset().top + button.outerHeight() -  $window.pageYOffset;
+                        var dropDownTopInTop = button.offset().top -  $window.pageYOffset;
 
                         if ($(window).height() < (dropDownTopInBottom + dropdown.height())) {
-                            dropdown.css('top', (dropDownTopInTop - dropdown.height() - $window.pageYOffset) + "px");
+                            dropdown.css('top', (dropDownTopInTop - dropdown.height()) + "px");
                         } else {
-                            dropdown.css('top', (dropDownTopInBottom - $window.pageYOffset) + "px");
+                            dropdown.css('top', (dropDownTopInBottom) + "px");
                         }
                         dropdown.css('left', button.offset().left + "px");
                     });
@@ -35,13 +35,13 @@
                         var dropdown = elm.find('.dropdown-menu-scoll:visible');
                         if (dropdown.length !== 0) {
 
-                            var dropDownTopInBottom = button.offset().top + button.outerHeight();
-                            var dropDownTopInTop = button.offset().top;
+                            var dropDownTopInBottom = button.offset().top + button.outerHeight() -  $window.pageYOffset;
+                            var dropDownTopInTop = button.offset().top -  $window.pageYOffset;
 
                             if ($(window).height() < (dropDownTopInBottom + dropdown.height())) {
-                                dropdown.css('top', (dropDownTopInTop - dropdown.outerHeight() - $window.pageYOffset) + "px");
+                                dropdown.css('top', (dropDownTopInTop - dropdown.outerHeight()) + "px");
                             } else {
-                                dropdown.css('top', (dropDownTopInBottom - $window.pageYOffset) + "px");
+                                dropdown.css('top', (dropDownTopInBottom) + "px");
                             }
                             dropdown.css('left', button.offset().left + "px");
 
@@ -53,12 +53,15 @@
                 },
             };
         }])
-        .directive('contentscroll', ['$document', '$window', function ($document, $window) {
+        .directive('contentscroll', ['$document', '$window', function($document, $window) {
             return {
                 restrict: 'C',
-                link: function (scope, elm) {
+                link: function(scope, elm) {
+                    var doc = angular.element($document);
+                    var bod = angular.element(doc[0].body);
+
                     // send message to children if scrolling
-                    elm.bind('scroll', function () {
+                    elm.bind('scroll', function() {
                         scope.$broadcast('contentScroll:scrolling',
                             {
                                 top: elm.offset().top,
@@ -67,13 +70,11 @@
                     });
 
                     // Window has scrolling also
-                    var doc = angular.element($document);
-                    doc.bind("scroll", function () {
-                        var bod = angular.element(doc[0].body);
+                    doc.bind("scroll", function() {
                         scope.$broadcast('contentScroll:scrolling',
                             {
-                                top: $window.pageYOffset,
-                                bottom: ($window.pageYOffset + bod.height()),
+                                top: elm.offset().top,
+                                bottom: (elm.offset().top + elm.height()),
                             });
                     });
                 },
